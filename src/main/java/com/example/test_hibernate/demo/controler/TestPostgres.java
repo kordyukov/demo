cc4f2e47fc6a5074e0ffee8e01c68c0a13872ce6
denis.kordiukov <denis.kordiukov@wilix.org>
2022-07-13 09:15:36 +0300

    add test metods

--- src/main/java/com/example/test_hibernate/demo/controler/TestPostgres.java
+++ src/main/java/com/example/test_hibernate/demo/controler/TestPostgres.java
@@ -4,13 +4,10 @@ import com.example.test_hibernate.demo.entity.Test;
 import com.example.test_hibernate.demo.repo.TestRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.server.ResponseStatusException;
 import service.TestPostgresService;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
